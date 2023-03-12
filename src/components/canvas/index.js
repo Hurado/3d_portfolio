2c53987d4861e48918dbdd7de639f7bcaaf314be
Hurado <piaszczynskiarkadiusz@gmail.com>
2023-03-12 19:18:26 +0100

    feat: :sparkles:

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,6 @@
 import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
+import Computers from "./Computers";
 import StarsCanvas from "./Stars";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
+export { EarthCanvas, BallCanvas, Computers, StarsCanvas };
