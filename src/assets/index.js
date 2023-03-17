9eaf6380bf1fe34cf6f05df9e1ea6e97250fbb3e
Hurado <piaszczynskiarkadiusz@gmail.com>
2023-03-17 09:26:22 +0100

    feat: :construction: Added Experience section with assets and layout

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,11 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+
+import studiofigura from "./company/studio-figura.png";
+import IBPM from "./company/IBPM.png"
+import OSKAR from "./company/OSKAR.png"
+import LaboPrint from "./company/LaboPrint.png"
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,11 +53,11 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
+  studiofigura,
+  IBPM,
+  OSKAR,
+  LaboPrint
 };
